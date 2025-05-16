65645a794ccd8cbf1991aec3e2a6fe7d8dcda6cc
deeev-cmd <vermadeepesh15@gmail.com>
2025-05-16 14:19:51 +0530

    Ui fix

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
           p={4}
         >
           {/* AI Model Selector - Using the original component */}
-          <Box mb={6} mt={5}>
+          <Box mb={6} mt={6}>
             <AIModelSelector />
           </Box>
           
@@ -158,7 +158,7 @@ function App() {
           p={4}
         >
           {/* Login button moved to top of right sidebar */}
-          <Box  mt={5} mb={4}>
+          <Box  mt={6} mb={4}>
             {!user ? (
               <Box bg="white" borderRadius="md" overflow="hidden">
                 <GoogleLogin
