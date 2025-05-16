cc75555b914137cf2b99062f93a7134ea3c68e08
deeev-cmd <vermadeepesh15@gmail.com>
2025-05-16 16:40:42 +0530

    Ui fix

--- src/components/PromptBox.js
+++ src/components/PromptBox.js
@@ -98,7 +98,7 @@ export default function PromptBox({ setMessages, user }) {
       w="100%"
       maxW="700px"
       position="absolute"
-      bottom="-10px"
+      bottom="-30px"
       left="50%"
       transform="translateX(-50%)"
     >
