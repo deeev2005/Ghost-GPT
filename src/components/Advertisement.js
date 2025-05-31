e25e1525423e71ef57326b71d5f7a647005fcc14
deeev-cmd <vermadeepesh15@gmail.com>
2025-05-31 22:16:20 +0530

    Ui fix

--- src/components/Advertisement.js
+++ src/components/Advertisement.js
@@ -23,6 +23,7 @@ export default function Advertisement() {
     >
       <Text fontSize="md" fontWeight="bold">
         🚀 If you don't get any response try 3-4 times and wait for some time to restart the server
+            Also when no model selected we will use last active model
       </Text>
     </Box>
   );
